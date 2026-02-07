b086b749ed56a179c2d8cd18828d9477447a3ebc
adityadorge <adityadorge07@gmail.com>
2026-02-07 11:52:43 +0530

    added ipynb

--- codeforce/codeforces.py
+++ codeforce/codeforces.py
@@ -1,5 +1,31 @@
-n, m, a = map(int, input().split())
+def solve():
+    import sys
+    input = sys.stdin.readline
 
-result = ((n + a - 1) // a) * ((m + a - 1) // a)
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        p = list(map(int, input().split()))
 
-print(result)
+        # pos[x] = index of value x in p
+        pos = [0] * (n + 1)
+        for i in range(n):
+            pos[p[i]] = i
+
+        # suffix_max[i] = max value in p[i:]
+        suffix_max = [0] * n
+        suffix_max[-1] = p[-1]
+        for i in range(n - 2, -1, -1):
+            suffix_max[i] = max(suffix_max[i + 1], p[i])
+
+        # find first position where improvement is possible
+        for i in range(n):
+            if p[i] != suffix_max[i]:
+                j = pos[suffix_max[i]]
+                p[i:j + 1] = reversed(p[i:j + 1])
+                break
+
+        print(*p)
+
+if __name__ == "__main__":
+    solve()
